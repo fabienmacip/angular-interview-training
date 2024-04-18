1a35a27220c5111242df5c7541ac337fc2b44749
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2024-04-18 15:43:14 +0200

    Exercice n°2 : Afficher une liste d’éléments issue d’un Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
 
@@ -7,22 +7,8 @@ import { delay } from 'rxjs/operators';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   bookList$: Observable<string[]> = of(['📙', '📕', '📗', '📘']).pipe(
     delay(1000)
   );
-
-  bookList: string[] = [];
-
-  ngOnInit(): void {
-    this.bookList$.subscribe((bookList) => (this.bookList = bookList));
-  }
 }
-
-//  bookList$: Observable<string[]> = of(['📙', '📕', '📗', '📘']).pipe(delay(1000));
-
-/*     `<img draggable="false" role="img" class="emoji" alt="📙" src="https://s.w.org/images/core/emoji/14.0.0/svg/1f4d9.svg">`,
-'<img draggable="false" role="img" class="emoji" alt="📕" src="https://s.w.org/images/core/emoji/14.0.0/svg/1f4d5.svg">',
-'<img draggable="false" role="img" class="emoji" alt="📗" src="https://s.w.org/images/core/emoji/14.0.0/svg/1f4d7.svg">',
-'<img draggable="false" role="img" class="emoji" alt="📘" src="https://s.w.org/images/core/emoji/14.0.0/svg/1f4d8.svg">',
-*/
